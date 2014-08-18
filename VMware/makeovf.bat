31bb281ad4a48352528a78f06dea304ab56a3939
bpt-dwaine <dwainek@bptfittings.com>
2014-08-18 15:58:53 -0400

    all

--- VMware/makeovf.bat
+++ VMware/makeovf.bat
@@ -1,4 +1,4 @@
 c:\
 cd\
-cd "C:\Program Files (x86)\VMware\VMware OVF Tool\"
-ovftool F:\LADemoOVF\LADemo.ovf "F:\LADemoVMX\Windows 7 x64 Pro R2.vmx"
+cd "C:\Program Files\VMware\VMware OVF Tool\"
+ovftool E:\Lanham 2013r2\LADemo2013r2.ovf "E:\Lanham 2013r2\LADemo2013r2VMX\Windows 7 x64 Pro R2.vmx"
